93155e61e796e6eb46190506bd9f499e3235de5e
AK101111 <arnavkansal@gmail.com>
2018-09-18 10:23:33 -0400

    add c style string exercise

---
+++ c_strings.c
@@ -0,0 +1,29 @@
+// STRING COPY
+// returns destination
+// destination : pointer to the location where source is to be copied.
+// destination should be atleast as long as source, with no memory overlap with source.
+char* strcpy(char *destination, const char* source){
+    // TODO: your code here
+}
+
+// STRING LENGTH
+// returns the length of the c-style NULL terminated string.
+size_t strlen(const char* string){
+    // TODO:
+}
+
+// STRING CONCATENATE
+// returns destination
+// the NULL char of destination is over-written by the first char of the source and a NULL char is included at the end of the new formed string
+char* strcat(char *destination, const char* source){
+    // TODO:
+}
+
+// STRING COMPARE
+// returns:
+// 0 if the contents of both the strings are equal
+// < 0 if the first char that does not match has a lower value in lhs
+// > 0 if the first char that does not match has a higher value in lhs
+int strcmp(const char* lhs, const char* rhs){
+    // TODO:
+}
